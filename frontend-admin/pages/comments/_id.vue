8a2d60f560d7e5d952deffb476940bde16fdc953
bottlehs <qudgns9@gmail.com>
2020-11-03 15:49:04 +0900

    댓글 라우터 추가 🍀

---
+++ frontend-admin/pages/comments/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6"> comments id {{ $route.params.id }} </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
