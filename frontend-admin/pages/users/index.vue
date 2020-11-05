b3de8b92936464669399a0f32f4f85624921180f
bottlehs <qudgns9@gmail.com>
2020-11-05 22:10:32 +0900

    users 수정

--- frontend-admin/pages/users/index.vue
+++ frontend-admin/pages/users/index.vue
@@ -1,10 +1,21 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6"> users </v-col>
+    <v-col cols="12">
+      <h2>유저</h2>
+      <v-btn @click="add">추가</v-btn>
+      <v-data-table
+        :headers="table.headers"
+        :items="table.items"
+        :items-per-page="pagenation.perPage"
+        class="elevation-1"
+      ></v-data-table>
+    </v-col>
   </v-row>
 </template>
 
 <script>
+import UsersService from '@/services/users.service.js'
+
 export default {
   layout: 'dashboard',
 
@@ -14,24 +25,56 @@ export default {
 
   props: {},
 
+  fetch(context) {
+    console.log(context + 'page fetch')
+  },
+
+  asyncData(context) {
+    console.log(context + 'page asyncData')
+  },
+
   data() {
     return {
+      table: {
+        headers: [
+          { text: 'email', value: 'email' },
+          { text: 'username', value: 'username' },
+          { text: 'firstname', value: 'firstname' },
+          { text: 'lastname', value: 'lastname' },
+          { text: 'languege', value: 'languege' },
+          { text: 'country', value: 'country' },
+          { text: 'createdAt', value: 'createdAt' },
+        ],
+        items: [],
+      },
+
       pagenation: {
         totalItems: 0,
-        items: [],
         totalPages: 0,
         currentPage: 0,
+        perPage: 10,
         wait: false,
       },
     }
   },
   fetchOnServer: false,
 
+  validate(context) {
+    console.log(context + 'page validate')
+    return true
+  },
+
   computed: {},
 
   watch: {},
 
+  beforeCreate() {},
+
   created() {},
+
+  beforeMount() {
+    this.fetch()
+  },
 
   mounted() {},
 
@@ -39,7 +82,33 @@ export default {
 
   destroyed() {},
 
-  methods: {},
+  methods: {
+    add() {
+      this.$router.push(`/users/edit`)
+    },
+    fetch() {
+      if (!this.pagenation.wait) {
+        this.pagenation.wait = true
+
+        const params = {}
+        UsersService.findAll(this.$axios, params).then(
+          (response) => {
+            const { data } = response
+            this.table.items = data.items
+            this.pagenation.totalItems = data.totalItems
+            this.pagenation.totalPages = data.totalPages
+            this.pagenation.currentPage = data.currentPage
+            this.pagenation.wait = false
+          },
+          (error) => {
+            alert(error)
+
+            this.pagenation.wait = false
+          }
+        )
+      }
+    },
+  },
 
   head() {
     return {
